f889f98bbb159047d0471b07c29f3b13d9d37d5b
Suwanto Ardi Winata <87982639+Justcallmesu@users.noreply.github.com>
2024-12-02 17:43:32 +0700

    fix (cookies): fixed cookies disappear after refresh

--- src/modules/common/config/cookies-config.ts
+++ src/modules/common/config/cookies-config.ts
@@ -1,13 +1,15 @@
 export function RefreshCookiesConfig() {
   return {
-    httpOnly: true,
+    secure: true,
     signed: true,
+    expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7),
   };
 }
 
 export function AccessCookiesConfig() {
   return {
-    httpOnly: true,
+    secure: true,
     signed: true,
+    expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7),
   };
 }
--- src/modules/common/function/generate-token.ts
+++ src/modules/common/function/generate-token.ts
@@ -6,9 +6,11 @@ import { GenerateTokenType } from '../enum/generate-token.enum';
 export default function GenerateToken(tokenType: string, payload: any) {
   switch (tokenType) {
     case GenerateTokenType.REFRESH_TOKEN:
-      return sign(payload, process.env.JWT_REFRESH_SECRET as string, {});
+      return sign(payload, process.env.JWT_REFRESH_SECRET as string, {
+        expiresIn: process.env.JWT_REFRESH_EXPIRATION
+      });
 
     case GenerateTokenType.ACCESS_TOKEN:
-      return sign(payload, process.env.JWT_ACCESS_SECRET as string, {});
+      return sign(payload, process.env.JWT_ACCESS_SECRET as string, {expiresIn: process.env.JWT_ACCESS_EXPIRATION,});
   }
 }
